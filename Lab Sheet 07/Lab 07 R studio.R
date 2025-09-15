84530451445780df7a082ce313d9dc6073de2429
IT24100197 <IT24100197@my.sliit.lk>
2025-09-15 18:54:14 +0530

    Lab 07

---
+++ Lab Sheet 07/Lab 07 R studio.R
@@ -0,0 +1,14 @@
+getwd()
+setwd('C://Users//asus//OneDrive//Desktop//IT24100197')
+# Problem 1
+punif(25, min=0, max=40) - punif(10, min=0, max=40)  
+
+# Problem 2
+pexp(2, rate = 1/3)
+
+# Problem 3.i
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+# Problem 3.ii
+qnorm(0.95, mean = 100, sd = 15)
+
